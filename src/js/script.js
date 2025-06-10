dac0aa0524e9a2a29b3ed6ec4b0e8fbee9346057
LucasHcp-1910 <lucasciiuu@gmail.com>
2025-06-10 20:17:54 -0300

    Initial commit

---
+++ src/js/script.js
@@ -0,0 +1,29 @@
+// SCRIPT CARROSSEL GALERIA
+document.querySelectorAll('.carousel').forEach((carousel) => {
+  let currentIndex = 0;
+  const imagesWrapper = carousel.querySelector('.carousel-images');
+  const images = imagesWrapper.querySelectorAll('img');
+  const totalImages = images.length;
+
+  const updateCarousel = () => {
+    imagesWrapper.style.transform = `translateX(-${currentIndex * 100}%)`;
+  };
+
+  carousel.querySelector('.prev-btn').addEventListener('click', () => {
+    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
+    updateCarousel();
+  });
+
+  carousel.querySelector('.next-btn').addEventListener('click', () => {
+    currentIndex = (currentIndex + 1) % totalImages;
+    updateCarousel();
+  });
+
+  setInterval(() => {
+    currentIndex = (currentIndex + 1) % totalImages;
+    updateCarousel();
+  }, 5000);
+});
+
+// SCRIPT TROCAR LOCALIZAÇÃO MAPS
+
