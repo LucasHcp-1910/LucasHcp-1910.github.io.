2a9072296648a3dc5927194ba5f187212754d668
LucasHcp-1910 <lucasciiuu@gmail.com>
2025-06-12 22:16:18 -0300

    correção carrossel e adição das legendas
    mudanças no carrosel e adição das legendas

--- src/js/script.js
+++ src/js/script.js
@@ -1,26 +1,56 @@
 // SCRIPT CARROSSEL GALERIA
-document.querySelectorAll('.carrosel').forEach((carousel) => {
-  let currentIndex = 0;
-  const imagesWrapper = carousel.querySelector('.carrosel-images');
-  const images = imagesWrapper.querySelectorAll('img');
-  const totalImages = images.length;
+var contador = 1;
+var imgsp1 ="./src/assets/imgbeco_do_batman.jpg";
+var imgsp2 ="./src/assets/rua_carmo.jpg";
+var imgsp3 ="./src/assets/catedral_se.jpg";
+var imgsp4 ="./src/assets/avenida paulista.jpg";
+var imgsp5 ="./src/assets/masp.jpg";
+var imgsp6 ="./src/assets/marginal_pinheiros.jpg";
+var legenda1 = "Beco do Batman ";
+var legenda2 = "Rua do Carmo";
+var legenda3 = "Catedral da Sé";
+var legenda4 = "Avenida Paulista";
+var legenda5 = "MASP";
+var legenda6 = "Marginal Pinheiros";
+var tempo=2000;
+var iniciar=setInterval("iniciando()", tempo)
 
-  const updateCarousel = () => {
-    imagesWrapper.style.transform = `translateX(-${currentIndex * 100}%)`;
-  };
+function iniciando()
+{
+  if (!document.images)
+    return
+  else
+    {
+      document.images["imgsp"].src=eval("imgsp"+contador);
+      
+      contador++
+      if (contador>6) contador=1
+    }
+  }
 
-  carousel.querySelector('.prev-btn').addEventListener('click', () => {
-    currentIndex = (currentIndex - 1 + totalImages) % totalImages;
-    updateCarousel();
-  });
+  function anterior()
+  {
+    clearInterval(iniciar);
+    contador = contador -1;
+    if (contador <0) 
+      {
+        contador = 6
+      }
+    document.images["imgsp"].src=eval("imgsp"+contador);
+    caption.innerHTML = eval("legenda" + contador);
 
-  carousel.querySelector('.next-btn').addEventListener('click', () => {
-    currentIndex = (currentIndex + 1) % totalImages;
-    updateCarousel();
-  });
+    iniciar=setInterval("iniciando()",tempo);
+}
 
-  setInterval(() => {
-    currentIndex = (currentIndex + 1) % totalImages;
-    updateCarousel();
-  }, 5000);
-});
+function proximo()
+{
+  clearInterval(iniciar);
+
+  contador = contador + 1;
+  if (contador >= imagens.length) contador = 0;
+
+  document.images["imgsp"].src = imagens[contador];
+
+  iniciar = setInterval(iniciando, tempo);
+}
+
